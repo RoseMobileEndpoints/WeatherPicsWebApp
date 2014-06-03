91cf84ae08c2f344e92d7fdd2189689fe1a268ec
David Fisher <fisherds@rose-hulman.edu>
2014-06-03 13:25:27 -0400

    Initial import

---
+++ models.py
@@ -0,0 +1,10 @@
+from google.appengine.ext import ndb
+
+# TODO: Convert this non-Endpoints version to use Endpoint Models!
+
+
+class WeatherPic(ndb.Model):
+    """ Model to store a WeatherPic. """
+    image_url = ndb.StringProperty()
+    caption = ndb.StringProperty()
+    last_touch_date_time = ndb.DateTimeProperty(auto_now=True)
